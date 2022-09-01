b5e6e20986d4d4e43007dc3c4886540a1d6d90c5
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-08-31 20:27:40 -0400

    update link to terms and privacy and add in privacy statement

--- server.js
+++ server.js
@@ -28,6 +28,16 @@ const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
+//===========Routes==================
+app.get('/terms', function (req, res) {
+  res.render('terms');
+});
+app.get('/privacy', function (req, res) {
+  res.render('privacy');
+});
+//===================================
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -35,5 +45,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening on Port ' + PORT ));
 });
