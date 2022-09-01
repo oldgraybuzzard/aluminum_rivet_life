db7175401de765fa7df897afd03b6b64e358bcbe
Kendall Felder <61666382+oldgraybuzzard@users.noreply.github.com>
2022-09-01 19:42:53 -0400

    remove search.js in server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const search = require('./public/javascript/search.js');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
