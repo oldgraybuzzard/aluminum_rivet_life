ffa5809682d309c735430c9ff35ea8107d7af671
oldgraybuzzard <k_felder@me.com>
2023-06-02 15:16:18 -0400

    get back up and running

--- db/db.sql
+++
@@ -1,2 +0,0 @@
-DROP DATABASE IF EXISTS aluminum_rivet_life_db;
-CREATE DATABASE aluminum_rivet_life_db;
---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS aluminum_rivet_life;
+
+CREATE DATABASE aluminum_rivet_life;
